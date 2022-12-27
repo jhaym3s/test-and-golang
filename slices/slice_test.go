a1bbc3ca3668b84b977f043fc6452a76cb2cfa3b
James <jhaymesifiok@gmail.com>
2022-12-27 17:07:26 +0100

    just understood len and make for slices

--- slices/slice_test.go
+++ slices/slice_test.go
@@ -1,16 +1,47 @@
 package slice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
+	t.Run("test sum for a fixed size", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+		assertCorrectAnswers(t, got, want)
+	})
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("test sum for a various size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+		assertCorrectAnswers(t, got, want)
+
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9, 5,1})
+	// So turns out that is one slice has more elements that it 
+	//will continue to add to last element other slides have in common
+	want := []int{3, 15}
+
+	// if got != want {
+	// 	t.Errorf("got %v want %v", got, want)
+	// }
+	///invalid operation: cannot compare got != want (slice can only be compared to nil)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v ", got, want)
 	}
 }
 
+func assertCorrectAnswers(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q wanted %q", got, want)
+	}
+}
--- slices/slices.go
+++ slices/slices.go
@@ -1,9 +1,29 @@
 package slice
 
-func Sum(numbers [5]int) int {
-	initialValue := 0
-	for i := 0; i < 5; i++ {
-		initialValue = initialValue + numbers[i]
+// func Sum(numbers [5]int) int {
+// 	initialValue := 0
+// 	for i := 0; i < 5; i++ {
+// 		initialValue = initialValue + numbers[i]
+// 	}
+// 	return initialValue
+// }
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
 	}
-	return initialValue
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	capacityOfReturnedSlice := len(numbersToSum)
+	//use "len" to get the capacity for the slice to be returned.
+	sums := make([]int,capacityOfReturnedSlice)
+	//above you are making a new slide with with the capacity gotten before
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
 }
