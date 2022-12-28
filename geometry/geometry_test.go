99899d19f86efb4c1c92522bee584bb3bc10f56c
James <jhaymesifiok@gmail.com>
2022-12-28 12:59:41 +0100

    another major refactor

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -14,9 +14,10 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func (t testing.TB, shape Shape, want float64)  {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
 		got := shape.Area()
-		
+
 		if got != want {
 			t.Errorf("got %g wanted %g", got, want)
 		}
@@ -24,30 +25,17 @@ func TestArea(t *testing.T) {
 
 	t.Run("cal area of circle", func(t *testing.T) {
 		circle := Circle{radius: 10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g wanted %g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 
 	t.Run("calc area of a rectangle", func(t *testing.T) {
 		rectangle := Rectangle{12., 6.}
-		got := rectangle.Area()
-		want := 72.
-		if got != want {
-			t.Errorf("got %.2f but wanted %.2f", got, want)
-		}
+
+		checkArea(t, rectangle, 72)
 	})
 
 	t.Run("calc area of a square", func(t *testing.T) {
 		square := Square{4.}
-		got:= square.Area()
-		want := 16.
-
-		if got != want {
-			t.Errorf("got %f wanted %f ", got, want)
-		}
+		checkArea(t,square,16)
 	})
 }
