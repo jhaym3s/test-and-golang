f08ec5208cd634bd019f447b959b80fcf351b0aa
James <jhaymesifiok@gmail.com>
2022-12-29 19:53:26 +0100

    pointers and errs

--- pointr/pointr_test.go
+++ pointr/pointr_test.go
@@ -1,39 +1,59 @@
 package pointr
 
 import (
-	"fmt"
 	"testing"
 )
 
+//https://dave.cheney.net/2016/04/27/dont-just-check-errors-handle-them-gracefully
 
 func TestWallet(t *testing.T) {
-	t.Run("test deposit", func(t *testing.T) {
-		wallet := Wallet{}
 
-		wallet.Deposit(10)
-	
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-	
-		fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-	
-		want := Bitcoin(10)
-	
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+	assertError:= func (t testing.TB,err error, want string)  {
+		t.Helper()
+		if err == nil {
+			t.Fatal("wanted an error but didn't get one")
+		}
+		if err.Error() != want {
+			t.Errorf("got %q want %q ", err, want)
+		}
+	}
+	assertNoError := func (t testing.TB, err error)  {
+		t.Helper()
+		if err != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
+	t.Run("test deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(10)
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("test withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(100)}
-		wallet.Withdraw(Bitcoin(50))
-		got :=  wallet.Balance()
-		want := Bitcoin(50)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		err := wallet.Withdraw(Bitcoin(50))
+		assertBalance(t, wallet, Bitcoin(50))
+		assertNoError(t,err)
 
 	})
 
-	
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+		assertBalance(t, wallet, startingBalance)
+		assertError(t,err, ErrInsufficientFunds.Error())
+
+		// if err == nil {
+		// 	t.Error("wanted an error but didn't get one")
+		// }
+	})
+
 }
