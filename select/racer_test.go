4456ac628fffc582c30013ed6338d783b29628b6
James <jhaymesifiok@gmail.com>
2023-01-02 19:39:33 +0100

    introducing select

--- select/racer_test.go
+++ select/racer_test.go
@@ -33,3 +33,30 @@ func TestRacer(t *testing.T) {
 	slowServer.Close()
 	fastServer.Close()
 }
+
+func TestRacer2(t *testing.T) {
+
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+	///By prefixing a function call with defer it will now call that function at the end of the containing function.
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
